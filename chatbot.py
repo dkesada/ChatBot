7e17d0b2e30284dd75370c508ca5e5572986835b
dkesada <dkesada@gmail.com>
2018-05-02 16:43:26 +0200

    Inicio del archivo principal

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,36 @@
 #author: David Quesada López
 
 import sys
+from textblob import TextBlob
+from random import randint
 
+def cargarCasas():
+	path = 'scraper/InfoPisos'
+	doc = open(path, 'r')
+	casas = doc.read().split('\n')
+	doc.close()
+	res = []
+	for c in casas:
+		c = c.split(';')
+		res.append(c)
+	
+	return res
+	
+def generarPregunta():
+	"""
+	Por ahora cojo una pregunta de las que tengo
+	"""
+	
+	doc = open('preguntas.txt', 'r')
+	res = doc.read().split(';')
+	doc.close()
+	return res[randint(0,len(res)-1)]
+
+def analizarPregunta(preg):
+	print(preg)
+	blob = TextBlob(preg)
+	blob.translate(to='en').tags
+	blob.translate(to='en').parse() #No me convence
 
 def main():
 	"""
@@ -15,8 +44,9 @@ def main():
 	4- Las características identificadas deben generar una serie de pisos válidos
 	5- Con los pisos válidos hay que generar una respuesta
 	"""
+	casas = cargarCasas() # Por ahora así. Puede sustituirse por una base de datos de mongo
 	pregunta = generarPregunta()
-	casa = analizarPregunta()
+	casa = analizarPregunta(pregunta)
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
