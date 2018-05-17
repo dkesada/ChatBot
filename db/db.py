f57bac268533818aa0ec0e437d36b745fb3c352d
dkesada <dkesada@gmail.com>
2018-05-17 16:28:28 +0200

    Base de datos Mongo

---
+++ db/db.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#author: David Quesada López
+
+from pymongo import MongoClient
+
+connection = MongoClient('localhost', 27017)
+db = connection.chatbot
+casas = db.casas
+
+def vacia():
+	"""Comprueba si la base de datos está vacía o no"""
+	ret = False
+	test = casas.find_one()
+	if test is None:
+		ret = True
+	return ret
+
+def guardarCasa(c):
+	"""Guarda una casa en la base de datos"""
+	casas.insert_one({'op':c[0], 'tipo':c[1], 'lugar':c[2], 'precio':c[3], 'tamano':c[4], 'habit':c[5], 'banos':c[6], 'estado':c[7], 'muebles':c[8],})
+
+
+#def obtenerSimilares(caracs):
+#	"""Obtiene pisos similares de la base de datos en función a las características introducidas."""
